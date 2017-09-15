c1ea79be0bfbd1544642db1ccd44bca7285dc7dc
5footerica <5footerica@gmail.com>
2017-09-15 16:15:52 -0700

    started assignment

--- usc-config.py
+++ usc-config.py
@@ -1,2 +1,36 @@
-def butts():
-    print "butts"
+""" textAdventure.py
+Erica Weems
+09/15/2017
+Text adventure game with functions and inventory
+"""
+#My global variables and initial values
+lives = 3
+points = 0
+points = [magenta, cyan, yellow, white]
+status = "alive"
+
+#My functions
+def start():
+
+def dead():
+    if status != alive:
+        print "You have died. You have " + lives + " left."
+            if lives > 0:
+                print "Let's try again."
+                start()
+            else:
+                again = raw_input("Like, suuuupppperrrrr dead. Wanna start over?")
+                if again.upper() == "Y" or again.upper() == "YES":
+                    print "Let's go!"
+                    start() #startover()?
+            else:
+                print "Goodbye!"
+                quit()
+
+#My loops
+
+
+
+
+#The rest
+start()
